3777b5a85fce53b61991853e8fe56dbe37b609ae
DashKiwi <Maxmooney08@gmail.com>
2024-11-12 17:00:13 +1300

    Added the setup file
    Added the Setup.bat file to install the requirements

---
+++ Setup.bat
@@ -0,0 +1,10 @@
+@echo off
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+@echo on
+pip install keyboard
+pip install pyscreenshot
+pip install pyperclip
+pip install ctypes
